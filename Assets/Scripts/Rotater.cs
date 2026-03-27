987b498220bd5248ed2a5a11131a42a7500837c5
Sergelongolous <matosse@sheridancollege.ca>
2026-03-27 16:51:27 -0400

    Testing Unity Event demo class

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnRotate()
+    {
+        Debug.Log("Make This Object Rotate");
+    }
+}
